bd83cf1309d502f1a29c799dcad6db20abc7510e
AleksanrKotlyar <kotlyar.alek@gmail.com>
2023-01-14 12:34:28 +0200

    fix path

--- contacts.js
+++ contacts.js
@@ -2,7 +2,7 @@ const fs = require("fs/promises");
 const path = require("node:path");
 const { v1 } = require("uuid");
 
-const contactsPath = path.join(__filename, "/../db/contacts.json");
+const contactsPath = path.join(__dirname, "/db/contacts.json");
 
 const listContacts = async () => {
 	try {
